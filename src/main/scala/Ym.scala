8d6349e3694cab7411a6874d006b6087a3b4aeb9
Yusuke Sunagawa <sunagawa@englegym.com>
2015-07-26 16:22:56 +0900

    added ymd

--- src/main/scala/Ym.scala
+++ src/main/scala/Ym.scala
@@ -1,14 +1,7 @@
 class Ym(y: Int, m: Int) extends Moment {
-  def toNum(): Int = {
-    "%04d%02d".format(y, m).toInt
-  }
-  def toStr(): String = {
-    "%04d%02d".format(y, m)
-  }
+  def toStr(): String = "%04d%02d".format(y, m)
+  def toNum(): Int = toStr().toInt
   def getYear(): Int = y
   def getMonth(): Int = m
-
-  override def getMoment(): Int = {
-    toNum()
-  }
+  override def getMoment(): Int = toNum()
 }
